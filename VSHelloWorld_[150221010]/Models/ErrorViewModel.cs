3d06824c17ddfe5e40b11de36c44b888208ecffe
TTTavares <43744305+TTTavares@users.noreply.github.com>
2018-10-03 14:39:32 +0100

    Adicionar arquivos de projeto.

---
+++ VSHelloWorld_[150221010]/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSHelloWorld__150221010_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
